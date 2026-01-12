de85fc4419b253d196c36c879baa39f2307db1c9
0x41 <76443517+stellarie@users.noreply.github.com>
2026-01-12 16:54:47 +0800

    added saving to local storage + styling fixes

--- aozora-viewer-server/aozora-viewer/build.gradle.kts
+++ aozora-viewer-server/aozora-viewer/build.gradle.kts
@@ -24,9 +24,6 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.jsoup:jsoup:1.22.1")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 kotlin {
--- aozora-viewer-server/aozora-viewer/src/main/resources/static/js/app.js
+++ aozora-viewer-server/aozora-viewer/src/main/resources/static/js/app.js
@@ -20,17 +20,29 @@ const waitForElement = (selector) => {
 
 const useAozoraLoader = () => {
 
+  const state = {
+    author: "",
+    title: "",
+    link: ""
+  }
+
   // for any css changes on the fly
   const init = () => {
     const loader = useAozoraLoader();
     document.getElementById("loading").style.display = "none";
     document.getElementById("reader").style["overflow-x"] = "unset";
+    state.author = "";
+    state.title = "";
+    state.link = "";
+
     return loader;
   }
 
   const load = (path) => {
     const reader = document.getElementById("reader");
     document.getElementById("loading").style.display = "block";
+    document.getElementById("library").style["display"] = "none";
+    document.getElementById("reader").style["display"] = "block";
     document.getElementById("reader").style["overflow-x"] = "unset";
 
     reader.innerHTML = "";
@@ -47,14 +59,73 @@ const useAozoraLoader = () => {
         reader.innerHTML = html;
       });
       waitForElement(".title").then(() => {
+        const pageTitle = document.getElementById("viewer-title");
+        const pageAuthor = document.getElementById("viewer-author");
+        const title = document.getElementsByClassName("title").item(0).textContent;
+        const author = document.getElementsByClassName("author").item(0).textContent;
+
+        pageTitle.innerHTML = title;
+        pageAuthor.innerHTML = author;
+        document.getElementById("reader").style["overflow-x"] = "scroll";
+        document.getElementsByClassName("title").item(0).scrollIntoView();
+        document.getElementById("loading").style.display = "none";
+
+        state.title = title;
+        state.author = author;
+        state.link = path;
         resolve();
       });
     })
   }
 
+  const save = () => {
+    if (!state.title || !state.author || !state.link) return;
+
+    const savedItems = JSON.parse(localStorage.getItem("savedItems")) || [];
+    if (savedItems.some(item => item.link === state.link)) return;
+
+    savedItems.push({
+      title: state.title,
+      author: state.author,
+      link: state.link
+    });
+    localStorage.setItem("savedItems", JSON.stringify(savedItems))
+    alert(`${state.title} by ${state.author} has been saved. \n\n${state.link}`);
+  }
+
+  const showLibrary = () => {
+    const savedItems = JSON.parse(localStorage.getItem("savedItems")) || [];
+    if (savedItems.length === 0) return;
+
+    document.getElementById("reader").style["display"] = "none";
+    const library = document.getElementById("library");
+    library.innerHTML = "";
+    library.style["display"] = "block";
+    const table = document.createElement("table");
+    table.classList.add("library-table");
+    table.innerHTML = `<tr><th>書名</th><th>作家</th><th></th></tr>`;
+
+    savedItems.forEach(item => {
+      const row = table.insertRow();
+      const titleCell = row.insertCell();
+      titleCell.innerHTML = item.title;
+      const authorCell = row.insertCell();
+      authorCell.innerHTML = item.author;
+      const readCell = row.insertCell();
+      const readButton = document.createElement("button");
+      readButton.addEventListener("click", () => loadPage(item.link));
+      readButton.classList.add("library-read-button");
+      readButton.innerHTML = "読む";
+      readCell.appendChild(readButton);
+    });
+    library.appendChild(table);
+  }
+
   return {
+    save,
     init,
-    load
+    load,
+    showLibrary
   };
 };
 
@@ -62,14 +133,13 @@ const loader = useAozoraLoader();
 loader.init();
 
 const loadPage = (path) => {
-  loader.load(path).then(() => {
-    const title = document.getElementById("viewer-title");
-    const author = document.getElementById("viewer-author");
-    title.innerHTML = document.getElementsByClassName("title").item(0).textContent;
-    author.innerHTML = document.getElementsByClassName("author").item(0).textContent;
-    document.getElementById("reader").style["overflow-x"] = "scroll";
-    document.getElementsByClassName("title").item(0).scrollIntoView();
-    document.getElementById("loading").style.display = "none";
-  });
+  loader.load(path);
 };
 
+const savePage = () => {
+  loader.save();
+}
+
+const showLibrary = () => {
+  loader.showLibrary();
+}
